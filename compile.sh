4b2527551c3d3d93711bb2a66f4b904ae8257973
Dastan <dastanbeksamatov@gmail.com>
2020-12-22 17:57:53 +0200

    Test workflow

--- compile.sh
+++ compile.sh
@@ -2,4 +2,6 @@ yarn build
 #!/usr/bin/env bash
 hex="0x"
 result=${hex}$( cat wasm-code )
-cat ./spec-files/customSpecRaw.json | jq --arg res "${result}" '.genesis.raw.top."0x3a636f6465" |= $res' | tee ./spec-files/customSpecRaw.json > /dev/null
+mv ./spec-files/customSpecRaw.json ./spec-files/temp-raw.json
+jq --arg res "${result}" '.genesis.raw.top."0x3a636f6465" |= $res' ./spec-files/temp-raw.json > ./spec-files/customSpecRaw.json
+rm ./spec-files/temp-raw.json
