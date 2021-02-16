0ffc4d8c45024cf3a6c03b6e3c9156f994230ddd
Dastan <dastanbeksamatov@gmail.com>
2021-02-16 14:25:57 +0200

    Test ci/cd (1)

--- .github/workflows/scripts/compile.sh
+++ .github/workflows/scripts/compile.sh
@@ -1,4 +1,5 @@
 npm install -g yarn
+yarn install
 npm run build
 node ./cli/dist/src/index.js spec --to=./spec-files/chain-spec.json
 node ./cli/dist/src/index.js spec --src=./spec-files/chain-spec.json
