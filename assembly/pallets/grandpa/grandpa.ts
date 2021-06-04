8676333f84df05d17e381c827ad57030b01f6bb7
Dastan <dastanbeksamatov@gmail.com>
2021-06-04 16:49:52 +0600

    Resolve tests

--- assembly/pallets/grandpa/grandpa.ts
+++ assembly/pallets/grandpa/grandpa.ts
@@ -1,21 +1,26 @@
 import { ByteArray } from "as-scale-codec";
 import { Storage, Utils } from "subsembly-core";
 
-export namespace GrandpaStorageEntries {}
+/**
+ * @descritption Storage entries for Grandpa
+ */
+export namespace GrandpaStorageEntries {
+}
 
 /**
  * @description Class representing GRANDPA module
  */
 export class Grandpa {
-    static readonly GRANDPA_AUTHORITIES: string = ":grandpa_authorities";
+    public static readonly GRANDPA_AUTHORITIES: string = ":grandpa_authorities";
 
     /**
      * @description Get GRANDPA authorities from the storage
-     * @returns concatenated list of Grandpa authorities
+     * concatenated list of Grandpa authorities
      */
     static _authorities(): u8[] {
         const entry = Storage.get(Utils.stringsToBytes([this.GRANDPA_AUTHORITIES], false));
-        const auths =  entry.isSome() ? (<ByteArray>entry.unwrap()).unwrap() : [];
+        const auths =  entry.isSome() ? (<ByteArray>entry.unwrap()).unwrap() : [0];
+        // first byte is option byte
         return auths;
     }
 
--- assembly/runtime/api/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./aura-api";
-export * from "./block-builder";
-export * from "./core";
-export * from "./grandpa-api";
-export * from "./others";
-export * from "./parachain-host";
