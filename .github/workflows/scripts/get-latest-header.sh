47b4992e5efd3dc982facdb8a8e5d3445932a2b8
Dastan <dastanbeksamatov@gmail.com>
2021-06-08 12:00:14 +0300

    Remove logs

--- .github/workflows/scripts/get-latest-header.sh
+++ .github/workflows/scripts/get-latest-header.sh
@@ -3,7 +3,7 @@ curl --location --request POST 'localhost:9933' \
 --header 'Content-Type: application/json' \
 --data-raw '{
     "jsonrpc": "2.0",
-    "method": "chain_getFinalizedHead",
+    "method": "chain_getHeader",
     "params": [],
     "id": 1
 }'
